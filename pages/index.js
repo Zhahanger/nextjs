029aa351d7e68efe01718fa54a1fcbc8392eaf94
Zhahanger <68742417+Zhahanger@users.noreply.github.com>
2020-12-02 20:05:01 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Calculator from '../components/calculator';
 export default function Home() {
 	return (
 		<div>
-		<img src="https://ic.pics.livejournal.com/kukmor/14257925/10039962/10039962_original.jpg"/>	
+			
 		</div>
 	);
 }
