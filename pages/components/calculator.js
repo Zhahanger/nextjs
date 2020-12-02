fd0d9a7bd068138d54617ce7c7e134bca45a8b59
Zhahanger <jkidiraliev@gmail.com>
2020-12-02 12:19:21 +0600

    fix icon

--- pages/components/calculator.js
+++ pages/components/calculator.js
@@ -203,6 +203,12 @@ const Text = styled.span`
 	cursor: ${({ toggle }) => (toggle ? 'pointer' : 'auto')};
 `;
 
+const Icon = ({ ...props }) => (
+	<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 492 492' {...props}>
+		<path d='M382.7 226.8l-219-219c-5-5-11.8-7.8-19-7.8s-14 2.8-19 7.9l-16.2 16a27 27 0 0 0 0 38.1L293.4 246l-184 184a26.8 26.8 0 0 0 0 38l16 16.2a26.9 26.9 0 0 0 38 0L382.8 265c5-5 7.8-11.9 7.8-19 0-7.3-2.7-14.1-7.8-19.2z' />
+	</svg>
+);
+
 const numberFormat = (num) => {
 	num = num.toString();
 	num = num.replace(/\s/g, '');
@@ -266,7 +272,7 @@ const BankInfoComponent = (props) => {
 					toggleAllItems(!showAllItems);
 				}}>
 				{showAllItems ? 'Скрыть' : 'Показать ещё банки'}
-				<ChevronIcon />
+				<Icon />
 			</Text>
 		</BankOffersContainer>
 	);
